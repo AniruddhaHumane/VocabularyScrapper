6591f59dcecd1bbd3a38b04ef7373889acc472db
Aniruddha Humane <ani.humane@outlook.com>
2019-02-06 20:44:08 +0530

    cleaed up

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # VocabularyScrapper
 Python code to scrap Vocabulary from Vocabulary.com
+
+### NOTE - Doing this is against their terms of your and you can get blocked. 
+### Use this code only for educational purpose
--- VocabScrapper.py
+++ VocabScrapper.py
@@ -11,34 +11,33 @@ import pandas as pd
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-WordList = pd.read_csv("WordList.txt", sep=", ")
+WordList = open("WordList.txt").read().split(", ")
 url = "https://www.vocabulary.com/dictionary/"
 print(len(WordList))
 
 FinalDataFrame = pd.DataFrame(columns=['Word','ShortDefinition','LongDefinition','Meaning','Synonyms','Antonyms'])
-checkWord = ["abstemious"]
-for word in WordList:
+checkWord = ["chastise"]
+for word in checkWord:
     page = urlopen(url+word)
     print("URL Opened")
 
     formattedPage = BeautifulSoup(page, "lxml")
     print("Page Formatted")
 
-    #print parsed_html.body.find_all('div', attrs={'class':'container'}).text
     word = formattedPage.find("h1", attrs={'class':'dynamictext'}).text
-
 
     shortDescription = formattedPage.find("p", attrs={'class':'short'}).text
 
     longDescription = formattedPage.find("p", attrs={'class':'long'}).text
-
-    #Definitions
 
     FinalDefs = []
     Synon = []
     Anton = []
 
     definitionGroups = formattedPage.find_all("div", attrs={'class':'group'})
+
+    # Sentences = formattedPage.find_all("div", attrs={'class':'sentence'})
+    # print(Sentences)
 
     for defGrp in definitionGroups:
         ordinals = defGrp.find_all("div", attrs={'class':'ordinal'})
@@ -53,43 +52,12 @@ for word in WordList:
                 FinalDefs.append(d)
             
             dls = ordinal.find_all("dl", attrs={'class':'instances'})
-            # print(dls)
-            # Syn = []
-            # synFlag = False
-            # Ant = [] 
-            # antFlag = False
             for dl in dls:
                 synAnt = dl.find("dt")
                 if synAnt.text == "Synonyms:":
                     Synon.append([ i.text for i in dl.find_all("a", attrs={'class':'word'})])
                 elif synAnt.text == "Antonyms:":
                     Anton.append([ i.text for i in dl.find_all("a", attrs={'class':'word'})])
-
-
-            # for i,row in enumerate(synAnt):
-            #     if row.text == "Synonyms:" or (row.text == "" and synFlag == True):
-            #         Syn.append(i)
-            #         synFlag = True
-            #     elif row.text == "Antonyms:" or (row.text == "" and antFlag == True):
-            #         synFlag = False
-            #         antFlag = True
-            #         Ant.append(i)
-            #     else:
-            #         synFlag = False
-            #         antFlag = False
-            # dds = ordinal.find_all("dd")
-            # for i,dd in enumerate(dds):
-            #     r1 = dd.find_all("a", attrs={'class':'word'})
-            #     for r in r1:
-            #         if i in Syn:
-            #             Synon.append(r.text)
-            #         if i in Ant:
-            #             Anton.append(r.text)
-
-    # if len(Synon) == 0:
-    #     Synon = ["Can't","find","any"]
-    # if len(Anton) == 0:
-    #     Anton = ["Can't","find","any"]
 
     print(word)
     print(shortDescription)
@@ -105,6 +73,7 @@ for word in WordList:
         'Meaning' : [', '.join(FinalDefs)],
         'Synonyms' : [', '.join(sum(Synon, []))],
         'Antonyms' : [', '.join(sum(Anton, []))]
+        # 'Sentence' : ['; '.join(Sentences)]
     })
 
     FinalDataFrame = pd.concat([FinalDataFrame,tempDf])
